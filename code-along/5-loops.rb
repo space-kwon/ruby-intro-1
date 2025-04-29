e3bd9a612fd777e940c463ab58cee64fbecd5788
Jungha Kwon <jungha.kwon@kellogg.northwestern.edu>
2025-04-29 13:42:59 +0000

    2

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,14 +3,14 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
-# index = 0
-# loop do
-#   puts "tacos!"
-#   if index == 10
-#     break
-#   end
-#   index = index + 1
-# end
+index = 0
+loop do
+  puts "tacos!"
+  if index == 10
+    break
+  end
+  index = index + 1
+end
 
 # Loop through tacos
 tacos = ["carnitas", "carne asada", "pollo", "pescado", "avocado", "tofu"]
